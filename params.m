4acbc1eb7549bb8b3626dcf22b6f6f6355d0712d
smitj19 <70969765+smitj19@users.noreply.github.com>
2025-11-17 16:05:54 -0500

    Update params.m
    just spacing

--- params.m
+++ params.m
@@ -16,21 +16,7 @@ q20 = X0(2);
 
 kp=4;
 Ts = 0.001;
-m1=0.095; 
-m2=0.024;
-L1= 0.1;
-L2= 0.129;
-l1 =L1/2;
-l2 =L2/2;
-g= 9.81;
-J1 = (1/12)*m1*L1^2;
-J2 = (1/12)*m2*L2^2;
-Rm=8.4;
-km= 0.042;
-b1= 0.001;
-b2 = 1e-5;
-Jb1 = J1+m1*l1^2+m2*L1^2;
-Jb2 = J2+m2*l2^2;
+
 %%
 %--------PHASE 3 STUFF----------
 P = load('ph.mat').Ph;
@@ -53,7 +39,7 @@ disp(sys_poles) %TASK 1
 %If real -ve poles -> stable, if real +ve poles -> unstable
 Pc = zeros(size(A,1), size(A,2));
 for N = 1:size(A,1)
-Pc(N,:) = A^(N-1)*B;
+    Pc(N,:) = A^(N-1)*B;
 end
 disp(rank(Pc)) %TASK 2
 
